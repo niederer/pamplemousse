bcd880c0c36900ff5498629138aa60562ed2c9ae
Carissa Niederer <cniederer@gmail.com>
2012-08-27 22:16:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pamplemousse::Application.config.secret_token = 'ed1b00bfaad53a90e5e72005ff8d270245333f538345c32a7ec7dd6b0e6412f567fb37ab7b001e9f89ad1eff62e0a26369004b63c157b9c84c63be9d3b1648e1'
