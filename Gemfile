cca81d1d2e74fcfab8372dd3488fb2bf02c75b1a
Carissa Niederer <cniederer@gmail.com>
2012-10-01 22:41:09 -0500

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -12,10 +12,16 @@ end
 
 # Gems used only for assets and not required
 # in production environments by default.
+
 group :assets do
-  gem 'sass-rails',   '~> 3.2.5'
+  gem 'compass-rails','~> 1.0.0.rc.2'
+  gem 'bootstrap-sass', '2.0.4'
+  gem 'compass-colors'
+  gem 'sassy-buttons'
+  gem 'sass-rails', '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.2'
   gem 'uglifier', '1.2.3'
+  # non-compass gems omitted for brevity
 end
 
 gem 'jquery-rails', '2.0.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Pamplemousse::Application.routes.draw do
   
+  get "users/new"
+
   # Static Pages
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
